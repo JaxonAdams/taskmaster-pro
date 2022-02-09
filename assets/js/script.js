ab2c86c81333a6cd129a013551f7139b601b19e6
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-09 10:32:59 -0700

    added delete dropzone functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,6 +234,21 @@ $(".card .list-group").sortable({
   }
 });
 
+// Trash drag and drop delete
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
